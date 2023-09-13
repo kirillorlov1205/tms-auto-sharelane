28d0a20913e62491985d4b575dc1b02b1214927c
Kiryl.Arlou <kirillorlov1205@gmail.com>
2023-09-13 12:27:10 +0300

    Assert fixes

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import support.Constants;
 
 public class SearchTest {
@@ -25,68 +29,71 @@ public class SearchTest {
 
     @AfterClass
     public void afterTest() {
-        driver.quit();
+//        driver.quit();
     }
 
     @Test(testName = "Verify searching by one-word book name")
     public void verifySearchingByOneWordBookName() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Gitanjali");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='Gitanjali ']")).isDisplayed());
+        WebElement expectedBookName = driver.findElement(By.xpath("//p[text()='Gitanjali ']"));
+        Assert.assertTrue(expectedBookName.isDisplayed());
     }
 
     @Test(testName = "Verify searching by multiple-word book name")
     public void verifySearchingByMultipleWordsBookName() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("The Power of Positive Thinking");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='The Power of Positive Thinking ']"))
-                .isDisplayed());
+        WebElement expectedBookName = driver.findElement(By.xpath("//p[text()='The Power of Positive " +
+                "Thinking ']"));
+        Assert.assertTrue(expectedBookName.isDisplayed());
     }
 
     @Test(testName = "Verify searching by one word from multiple-word book name")
     public void verifySearchingByOneWordFromMultipleWordBookName() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Power");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='The Power of Positive Thinking ']"))
-                .isDisplayed());
+        WebElement expectedBookName = driver.findElement(By.xpath("//p[text()='The Power of Positive " +
+                "Thinking ']"));
+        Assert.assertTrue(expectedBookName.isDisplayed());
     }
 
     @Test(testName = "Verify searching by a part of a word from book name")
     public void verifySearchingByPartOfWordFromBookName() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Powe");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertEquals(driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR)
-                .getText(), Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
+        String expectedConformationMessageText = driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR).getText();
+        Assert.assertEquals(expectedConformationMessageText, Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
     }
 
     @Test(testName = "Verify searching by word with less than minimal limit characters")
     public void verifySearchingByWordWithLessThanMinLimitCharacters() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Pow");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertEquals(driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR)
-                .getText(), Constants.WORD_WITH_LESS_THAN_MIN_LIMIT_CHARACTERS_WARNING_MESSAGE);
+        String expectedConformationMessageText = driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR).getText();
+        Assert.assertEquals(expectedConformationMessageText, Constants.WORD_WITH_LESS_THAN_MIN_LIMIT_CHARACTERS_WARNING_MESSAGE);
     }
 
     @Test(testName = "Verify searching by empty string")
     public void verifySearchingByEmptyString() {
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertEquals(driver.findElement(Constants.WARNING_MESSAGE_LOCATOR)
-                .getText(), Constants.EMPTY_SEARCH_WARNING_MESSAGE);
+        String expectedWarningMessageText = driver.findElement(Constants.WARNING_MESSAGE_LOCATOR).getText();
+        Assert.assertEquals(expectedWarningMessageText, Constants.EMPTY_SEARCH_WARNING_MESSAGE);
     }
 
     @Test(testName = "Verify searching by author first name")
     public void verifySearchingByAuthorFirstName() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Twain");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertEquals(driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR)
-                .getText(), Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
+        String expectedConformationMessageText = driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR).getText();
+        Assert.assertEquals(expectedConformationMessageText, Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
     }
 
     @Test(testName = "Verify searching by author last name")
     public void verifySearchingByAuthorLastname() {
         driver.findElement(Constants.SEARCH_FIELD_LOCATOR).sendKeys("Mark");
         driver.findElement(Constants.SEARCH_BUTTON).click();
-        Assert.assertEquals(driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR)
-                .getText(), Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
+        String expectedConformationMessageText = driver.findElement(Constants.CONFORMATION_MESSAGE_LOCATOR).getText();
+        Assert.assertEquals(expectedConformationMessageText, Constants.NO_BOOKS_FOUND_SEARCH_WARNING_MESSAGE);
     }
 }
